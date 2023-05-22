d1b0735ddfeeeddff0917ca5ec71a35cd96eb3f4
Ravi Tailor <ravitailor148@gmail.com>
2023-05-22 14:03:59 +0530

    first commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,29 +2,30 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import '../static/form.css';
+
 const UserForm = () => {
-  const navigate= useNavigate();
+  const navigate = useNavigate();
   const [name, setName] = useState('');
   const [dob, setDob] = useState('');
   const [email, setEmail] = useState('');
-  const [phone_number, setphone_number] = useState('');
+  const [phone_number, setPhoneNumber] = useState('');
+  const [error, setError] = useState('');
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-  
-     if (!name || !dob || !email || !phone_number) {
-      alert('Please fill in all the fields');
-      navigate('/user-form')
+
+    if (!name || !dob || !email || !phone_number) {
+      setError('Please fill in all the fields');
       return;
     }
+
     const currentDate = new Date();
     const selectedDate = new Date(dob);
     const ageDiff = currentDate - selectedDate;
     const ageInYears = Math.floor(ageDiff / (1000 * 60 * 60 * 24 * 365));
-    
+
     if (ageInYears < 18) {
-      alert('You must be at least 18 years old');
-      navigate('/user-form')
+      setError('You must be at least 18 years old');
       return;
     }
 
@@ -34,50 +35,81 @@ const UserForm = () => {
       email,
       phone_number
     };
-  
+
     try {
-      // POST request to the Django backend API
-      const resp = await axios.post('https://ravi777345.pythonanywhere.com/api/user-form/', userdata);
+      const resp = await axios.post(
+        'https://ravi777345.pythonanywhere.com/api/user-form/',
+        userdata
+      );
       console.log(resp.data);
 
-      window.alert('Success. We send you a mail for becoming a part of our precious family. ')
-      navigate('/submitted-form')
-
-
+      window.alert('Success. We sent you an email for becoming a part of our precious family.');
+      navigate('/submitted-form');
     } catch (error) {
       console.error(error);
       // Handle the error response, display error message, etc.
     }
-  
-    // Reset form fields
+
+    // Reset form fields and error message
     setName('');
     setDob('');
     setEmail('');
-    setphone_number('');
+    setPhoneNumber('');
+    setError('');
   };
-  
 
   return (
     <div className="container">
       <span className="form__title">User Forms</span>
+      {error && <div className="form__error">{error}</div>}
       <form className="form" onSubmit={handleSubmit}>
-      <div class="form__group">
-					<input type="text" placeholder="Name" required className="form__input"  value={name} onChange={(event) => setName(event.target.value)} />
-					<span class="form__bar"></span>
-				</div>
-      <div class="form__group">
-					<input type="date" placeholder="Date of Birth" required className="form__input"  value={dob} onChange={(event) => setDob(event.target.value)} />
-					<span class="form__bar"></span>
-				</div>
-      <div class="form__group">
-					<input type="text" placeholder="Email" required className="form__input" value={email} onChange={(event) => setEmail(event.target.value)} />
-					<span class="form__bar"></span>
-				</div>
-      <div class="form__group">
-					<input type="tel" placeholder="Phone" required className="form__input" value={phone_number} onChange={(event) => setphone_number(event.target.value)} />
-					<span class="form__bar"></span>
-				</div>
-        <button type="submit" class="form__button">Sign up</button>
+        <div className="form__group">
+          <input
+            type="text"
+            placeholder="Name"
+            required
+            className="form__input"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <span className="form__bar"></span>
+        </div>
+        <div className="form__group">
+          <input
+            type="date"
+            placeholder="Date of Birth"
+            required
+            className="form__input"
+            value={dob}
+            onChange={(event) => setDob(event.target.value)}
+          />
+          <span className="form__bar"></span>
+        </div>
+        <div className="form__group">
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            className="form__input"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <span className="form__bar"></span>
+        </div>
+        <div className="form__group">
+          <input
+            type="tel"
+            placeholder="Phone"
+            required
+            className="form__input"
+            value={phone_number}
+            onChange={(event) => setPhoneNumber(event.target.value)}
+          />
+          <span className="form__bar"></span>
+        </div>
+        <button type="submit" className="form__button">
+          Sign up
+        </button>
       </form>
     </div>
   );
